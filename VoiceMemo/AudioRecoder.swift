237eba845ea9bbb67a4525c4abb418e9ec981ffe
entaku <entaku19890818@gmail.com>
2022-09-05 05:57:11 +0900

    change Audio setting

--- VoiceMemo/AudioRecoder.swift
+++ VoiceMemo/AudioRecoder.swift
@@ -94,7 +94,7 @@ private actor AudioRecorder {
           recorder.wrappedValue.stop()
         }
 
-        try AVAudioSession.sharedInstance().setCategory(.record, mode: .default)
+          try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: .defaultToSpeaker)
         try AVAudioSession.sharedInstance().setActive(true)
         self.recorder?.record()
       } catch {
