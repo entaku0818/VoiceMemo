ab8cdbe17b91800c13761967f11ff2480f59f3ca
entaku <entaku19890818@gmail.com>
2022-10-09 22:54:18 +0900

    add firebase analitics

--- VoiLog/VoiceMemoApp.swift
+++ VoiLog/VoiceMemoApp.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 import ComposableArchitecture
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct VoiceMemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var voiceMemos:[VoiceMemoState] = []
     let DocumentsPath = NSHomeDirectory() + "/Documents"
     init() {
