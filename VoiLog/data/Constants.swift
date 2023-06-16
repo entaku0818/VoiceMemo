ca3e0fd87c4259f4c35f16374dbe4ded6decc2e3
entaku <entaku19890818@gmail.com>
2023-06-16 09:03:07 +0900

    サンプリング周波数を追加

--- VoiLog/data/Constants.swift
+++ VoiLog/data/Constants.swift
@@ -14,4 +14,24 @@ class Constants {
         case AAC
     }
 
+    enum SamplingFrequency: Int, CaseIterable {
+        case hz11000 = 11000
+        case hz22000 = 22000
+        case hz44100 = 44100
+        case hz88200 = 88200
+
+        var stringValue: String {
+            switch self {
+            case .hz11000:
+                return "11,000Hz"
+            case .hz22000:
+                return "22,000Hz"
+            case .hz44100:
+                return "44,100Hz"
+            case .hz88200:
+                return "88,200Hz"
+            }
+        }
+    }
+
 }
