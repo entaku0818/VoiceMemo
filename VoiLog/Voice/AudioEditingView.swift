72564a8ef33a56da4aa7d0e07b7c477a4805243f
entaku <entaku19890818@gmail.com>
2023-06-25 13:02:30 +0900

    Audio編集機能追加

---
+++ VoiLog/Voice/AudioEditingView.swift
@@ -0,0 +1,20 @@
+//
+//  AudioEditingView.swift
+//  VoiLog
+//
+//  Created by 遠藤拓弥 on 25.6.2023.
+//
+
+import SwiftUI
+
+struct AudioEditingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AudioEditingView_Previews: PreviewProvider {
+    static var previews: some View {
+        AudioEditingView()
+    }
+}
