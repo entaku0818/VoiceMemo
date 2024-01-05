79bab2202c61462defe005a8b9c11a4c46ef5ae5
entaku <entaku19890818@gmail.com>
2024-01-06 00:09:51 +0900

    fix

--- VoiLog/Store/IAPManager.swift
+++ VoiLog/Store/IAPManager.swift
@@ -3,29 +3,44 @@ import StoreKit
 class IAPManager: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
     static let shared = IAPManager()
 
+    private var currentContinuation: CheckedContinuation<SKProduct, Error>?
+
+    enum IAPError: Error {
+        case cannotMakePayments
+        case productNotFound
+    }
+
     private override init() {
         super.init()
         SKPaymentQueue.default().add(self)
     }
 
-    // TODO: async/awaitにする
-    func startPurchase(productID: String) {
-        if SKPaymentQueue.canMakePayments() {
-            let request = SKProductsRequest(productIdentifiers: Set([productID]))
-            request.delegate = self
-            request.start()
-        } else {
+    func startPurchase(productID: String) async throws {
+        guard SKPaymentQueue.canMakePayments() else {
             // ユーザーが購入を行えない場合の処理
+            throw IAPError.cannotMakePayments
+        }
+
+        let product = try await fetchProduct(productIdentifier: productID)
+        buyProduct(product)
+    }
+
+    private func fetchProduct(productIdentifier: String) async throws -> SKProduct {
+        return try await withCheckedThrowingContinuation { continuation in
+            let request = SKProductsRequest(productIdentifiers: Set([productIdentifier]))
+            request.delegate = self
+            self.currentContinuation = continuation
+            request.start()
         }
     }
 
-    // TODO: async/awaitにする
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
         if let product = response.products.first {
-            buyProduct(product)
+            currentContinuation?.resume(returning: product)
         } else {
-            // 商品が見つからない場合の処理
+            currentContinuation?.resume(throwing: IAPError.productNotFound)
         }
+        currentContinuation = nil
     }
 
     private func buyProduct(_ product: SKProduct) {
